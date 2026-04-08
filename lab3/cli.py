3c1542c720734a81f044f13a778f4a671231b6e2
igory67 <etoyadadaa@gmail.com>
2026-04-08 11:58:30 +0300

    should be final +-

--- lab3/cli.py
+++ lab3/cli.py
@@ -90,23 +90,25 @@ def create_record():
 
     try:
         year = int(year)
+        
+        country = country[:30]
 
         if not check_year(year):
             #print("Ошибка: год должен быть в диапазоне 1985-2016.")
             return
             
         population = int(population)
-        if not check_int(population):
+        if not check_int(population) and not check_empty(population):
             print("Ошибка: население должно быть больше 0.")
             return
             
         suicides_no = int(suicides_no)
-        if not check_int(suicides_no):
+        if not check_int(suicides_no) and not check_empty(suicides_no):
             print("Ошибка: число суицидов не может быть отрицательным.")
             return
             
         suicides_100k_pop = float(suicides_100k_pop)
-        if not check_int(suicides_100k_pop):
+        if not check_int(suicides_100k_pop) and not check_empty(suicides_100k_pop):
             print("Ошибка: показатель суицидов не может быть отрицательным.")
             return
         
@@ -114,11 +116,11 @@ def create_record():
         print("Ошибка: проверьте типы данных (год, население, суициды - целые числа, показатель - число).")
         return
 
-    if not check_sex(sex):
+    if not check_sex(sex) and not check_empty(sex):
         # print("Ошибка: пол должен быть 'male' или 'female'.")
         return
     
-    if not check_age(age): # not in valid_ages:
+    if not check_age(age) and not check_empty(age): # not in valid_ages:
         # print(f"Ошибка: возрастная группа '{age}' недопустима.")
         # print(f"Допустимые значения: {', '.join(valid_ages)}")
         return
@@ -155,6 +157,11 @@ def check_id(rid):
         return False
     # return True
 
+def check_empty(attr):
+    if not attr:
+        print("Не может быть пустой строкой")
+        return False
+    return True 
 
 def update_record():
     
@@ -163,13 +170,99 @@ def update_record():
         #print("ID не может быть пустым.")
         return
     
-    print("Введите обновляемые поля в JSON (можно не все, например {\"suicides_no\": 600})")
-    data_str = input("JSON: ").strip()
-    try:
-        data = json.loads(data_str)
-    except json.JSONDecodeError:
-        print("Ошибка: неверный JSON.")
-        return
+    chosen_changes = {}
+
+    while (True):
+        print("Что хотите поменять? " \
+        "0 - выход" \
+        "1 - country" \
+        "2 - year" \
+        "3 - sex" \
+        "4 - age" \
+        "5 - suicides_no" \
+        "6 - population" \
+        "7 - suicides/100k pop")
+        decision = input("Выбор: ").strip()
+        if decision == 0:
+            break
+
+        elif decision == 1:
+            country = input("Введите название страные (длина <= 30)").strip()
+            if not check_empty(country):
+                continue
+            country = country[:30]
+            chosen_changes["country"] = country
+
+        elif decision == 2:
+            year = input("Год (year, 1985-2016): ").strip()
+            try:
+                year = int(year)
+                if not check_year(year):
+                    continue
+                chosen_changes["year"] = year
+            except ValueError:
+                print("Ошибка с типами данных в числах.")
+                continue 
+        elif decision == "3":
+            sex = input("Пол (male/female): ").strip().lower()
+            if not check_sex(sex):
+                continue
+            chosen_changes["sex"] = sex
+            
+        elif decision == "4":
+            valid_ages = ['5-14 years', '15-24 years', '25-34 years', 
+                            '35-54 years', '55-74 years', '75+ years']
+            print(f"Возрастные группы (age): {', '.join(valid_ages)}")
+            age = input("Возрастная группа: ").strip()
+            try:
+                age = int(age)
+                if not check_age(age):
+                    continue
+                chosen_changes["age"] = age
+            except ValueError:
+                print("Ошибка с типами данных в числах.")
+                continue 
+        elif decision == "5":
+            suicides_str = input("Число суицидов (suicides_no, >=0): ").strip() 
+            try:
+                suicides_no = int(suicides_str)
+                if not check_int(suicides_no) and not check_empty(suicides_no):
+                    continue
+                chosen_changes["suicides_no"] = suicides_no
+            except ValueError:
+                print("Ошибка с типами данных в числах.")
+                continue 
+        elif decision == "6":
+            pop_str = input("Население (population, >0): ").strip()
+            try:
+                population = int(pop_str)
+                if not check_int(population) and not check_empty(population):
+                    continue
+                chosen_changes["population"] = population
+            except ValueError:
+                print("Ошибка с типами данных в числах.")
+                continue 
+        elif decision == "7":
+            rate_str = input("Суицидов на 100k населения (suicides_100k_pop, >=0): ").strip()
+            try:
+                rate = float(rate_str)
+                if not check_int(rate):
+                    continue
+                chosen_changes["suicides_100k_pop"] = rate
+            except ValueError:
+                print("Ошибка с типами данных в числах.")
+                continue 
+        else:
+            continue
+
+        # '', 'year', 'sex', 'age', 'suicides_no', 'population', 'suicides/100k pop'
+    # print("Введите обновляемые поля в JSON (можно не все, например {\"suicides_no\": 600})")
+    # data_str = input("JSON: ").strip()
+    # try:
+    #     data = json.loads(data_str)
+    # except json.JSONDecodeError:
+    #     print("Ошибка: неверный JSON.")
+    #     return
     resp = requests.put(f"{BASE_URL}/suicides/{rid}", json=data)
     print_response(resp)
 
@@ -182,7 +275,7 @@ def delete_record():
         return
     resp = requests.delete(f"{BASE_URL}/suicides/{rid}")
     print_response(resp)
-
+    return
 
 def total_by_year():
     
